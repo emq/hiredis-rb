ca1ac3af3bc1870a7c3983f5e2134ee82d1b47ec
Rafal Wojsznis <rafal.wojsznis@gmail.com>
2014-03-16 20:22:27 +0100

    Freebsd fix

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+MAKE_COMMAND = Config::CONFIG['host_os'].downcase =~ /bsd|solaris/ ? "gmake" : "make"
 
 hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
 unless File.directory?(hiredis_dir)
@@ -9,7 +10,7 @@ unless File.directory?(hiredis_dir)
 end
 
 # Make sure hiredis is built...
-system("cd #{hiredis_dir} && make static")
+system("cd #{hiredis_dir} && #{MAKE_COMMAND} static")
 
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
