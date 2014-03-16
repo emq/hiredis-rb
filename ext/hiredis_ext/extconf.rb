3798cae4de523e9508c50048f5fc841ce3d4d1aa
Rafal Wojsznis <rafal.wojsznis@gmail.com>
2014-03-16 20:27:14 +0100

    Or maybe this way

--- ext/hiredis_ext/extconf.rb
+++ ext/hiredis_ext/extconf.rb
@@ -1,7 +1,6 @@
 require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
-MAKE_COMMAND = Config::CONFIG['host_os'].downcase =~ /bsd|solaris/ ? "gmake" : "make"
 
 hiredis_dir = File.expand_path(File.join(File.dirname(__FILE__), %w{.. .. vendor hiredis}))
 unless File.directory?(hiredis_dir)
@@ -10,7 +9,12 @@ unless File.directory?(hiredis_dir)
 end
 
 # Make sure hiredis is built...
-system("cd #{hiredis_dir} && #{MAKE_COMMAND} static")
+gnu_make = system("make --version 2>/dev/null | grep 'GNU Make' > /dev/null")
+if gnu_make
+  system("cd #{hiredis_dir} && make static")
+else
+  system("cd #{hiredis_dir} && gmake static")
+end
 
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
